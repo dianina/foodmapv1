1a2fcbfc4c32c3a0450afaed0d2ef4615e2eaa65
dianina <dianachcastro@yahoo.es>
2017-12-13 10:59:05 -0300

    2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,49 @@
 $(document).ready(function(){
 
- $('.loader').delay(3000).fadeOut("slow");
+/*
+*Loader
+*/
+ $('.loader').delay(2000).fadeOut("slow");
 
+/*
+*Paso de screen splash a contenido
+*/
+setTimeout(function(){
+    $('.loader').show();
+    }, 2000);
 
+/*
+*Filtro de búsqueda
+*/
 
+	$("#pasabocas").show();
+	$("#ffood").hide();
+	$("#gourmet").hide();
+	$("#exotica").hide();
+	$("#postres").hide();
 
+	$("#chile").click(function(){
+		$(".chilena").show();
+		$("#varias").hide();
+		$(".colombianas").hide();
+		$(".mexicanas").hide();
+	
+	})
 
+	$("#colombia").click(function(){
+		$(".chilena").hide();
+		$("#varias").hide();
+		$(".colombianas").show();
+		$(".mexicanas").hide();
+	
+	})
+
+	$("#mexico").click(function(){
+		$(".chilena").hide();
+		$("#varias").hide();
+		$(".colombianas").hide();
+		$(".mexicanas").show();
+	
+	})
 
 });
